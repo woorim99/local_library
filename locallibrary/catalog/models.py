8a45d69bef1522aff945e55312260c5820216a90
woorim99 <u_rim@naver.com>
2020-06-02 14:28:14 +0900

    part6

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+from django.urls import reverse
+
 class MyModelName(models.Model):
     """A typical class defining a model, derived from the Model class."""
 
@@ -86,12 +88,13 @@ class BookInstance(models.Model):
         help_text='Book availability',
     )
 
-class Meta:
-    ordering = ['due_back']
+    class Meta:
+        ordering = ['due_back']
 
-    def __str__(self):
-        """String for representing the Model object."""
-        return f'{self.id} ({self.book.title})'
+        def __str__(self):
+            """String for representing the Model object."""
+            return f'{self.id} ({self.book.title})'
+
 
 class Author(models.Model):
     """Model representing an author."""
@@ -99,6 +102,15 @@ class Author(models.Model):
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
     date_of_death = models.DateField('Died', null=True, blank=True)
+
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
+
+    class Meta:
+        ordering = ['last_name']
 
 class Meta:
     ordering = ['last_name', 'first_name']
--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from catalog import views
 
 
-urlpatterns = [
-
-]
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 ]
